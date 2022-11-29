2fe7e6287d2194cd3137025d7c952b42956ccb17
Jochen Klar <mail@jochenklar.de>
2022-11-29 12:23:09 +0100

    Add setup.cfg

--- django_datacite/__init__.py
+++ django_datacite/__init__.py
@@ -1,10 +1,3 @@
-__title__ = 'django-datacite'
-__version__ = '1.0'
-__author__ = 'Jochen Klar'
-__email__ = 'jochen.klar@pik-potsdam.de'
-__license__ = 'MIT'
-__copyright__ = 'Copyright (c) 2022 Potsdam Institute for Climate Impact Research'
-
-VERSION = __version__
+VERSION = __version__ = '1.0.0'
 
 default_app_config = 'django_datacite.apps.DataciteConfig'
--- setup.py
+++ setup.py
@@ -1,35 +1,3 @@
-import re
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-with open('django_datacite/__init__.py') as f:
-    metadata = dict(re.findall(r'__(.*)__ = [\']([^\']*)[\']', f.read()))
-
-with open('requirements.txt') as f:
-    install_requires = f.readlines()
-
-setup(
-    name=metadata['title'],
-    version=metadata['version'],
-    author=metadata['author'],
-    author_email=metadata['email'],
-    maintainer=metadata['author'],
-    maintainer_email=metadata['email'],
-    license=metadata['license'],
-    url='https://github.com/ISI-MIP/django-datacite',
-    description=u'',
-    long_description=open('README.md').read(),
-    install_requires=install_requires,
-    classifiers=[
-        # https://pypi.org/classifiers/
-        'Development Status :: 5 - Production/Stable',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Operating System :: OS Independent',
-        'License :: OSI Approved :: MIT License',
-    ],
-    packages=find_packages(),
-    include_package_data=True
-)
+setup()
