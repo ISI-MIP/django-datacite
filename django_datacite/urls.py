d784a183d2c0cf2c322c7d6b6f9c85298ba37a3b
Jochen Klar <mail@jochenklar.de>
2023-07-12 18:29:56 +0200

    Add resource_bibtex to urls.py

--- django_datacite/urls.py
+++ django_datacite/urls.py
@@ -1,11 +1,12 @@
 from django.urls import re_path
 
-from django_datacite.views import resource, resource_json, resource_xml
+from django_datacite.views import resource, resource_json, resource_xml, resource_bibtex
 
 app_name = 'django_datacite'
 
 urlpatterns = [
     re_path(r'^(?P<identifier>\d{2}\.\d+\/[A-Za-z0-9_.\-\/]+).xml$', resource_xml, name='resource_xml'),
     re_path(r'^(?P<identifier>\d{2}\.\d+\/[A-Za-z0-9_.\-\/]+).json$', resource_json, name='resource_json'),
+    re_path(r'^(?P<identifier>\d{2}\.\d+\/[A-Za-z0-9_.\-\/]+).bib$', resource_bibtex, name='resource_bibtex'),
     re_path(r'^(?P<identifier>\d{2}\.\d+\/[A-Za-z0-9_.\-\/]+)', resource, name='resource')
 ]
