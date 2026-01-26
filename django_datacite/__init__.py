15a60b8ebc492984e58dd7b2b48f26773418064d
Jochen Klar <mail@jochenklar.de>
2026-01-26 13:00:29 +0100

    Update pyproject.toml and add dynamic versioning

--- django_datacite/__init__.py
+++ django_datacite/__init__.py
@@ -1,3 +1,10 @@
-VERSION = __version__ = '1.0.2'
+from importlib.metadata import PackageNotFoundError
+from importlib.metadata import version as _version
+
+try:
+    VERSION = __version__ = _version(__package__)
+except PackageNotFoundError:
+    VERSION = __version__ = "0.0.0+unknown"
+
 
 default_app_config = 'django_datacite.apps.DataciteConfig'
